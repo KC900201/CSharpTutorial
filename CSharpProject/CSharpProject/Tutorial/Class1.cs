4a81cf5819dd20bbc32eb0184917610a05939741
kc900201 <nkc900201@gmail.com>
2020-09-06 16:59:40 +0900

    First upload of tutorial project. Refer to Rob Miles C# programming workbook - 09062020

---
+++ CSharpProject/CSharpProject/Tutorial/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpProject.Tutorial
+{
+    class Class1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Lola()
+        {
+            float a, b, c;
+            Class1 aClass = new Class1();
+
+            Console.WriteLine("Please enter an integer a: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter an integer b: ");
+            b = int.Parse(Console.ReadLine());
+
+            c = aClass.Add((int)a, (int)b); // typecasting example of a and b
+
+            Console.WriteLine("Result of adding: " + c.ToString());
+
+        }
+    }
+}
---
+++ CSharpProject/CSharpProject/Tutorial/Tutorial_csharp.cs
@@ -0,0 +1,75 @@
+﻿/**
+Created on 06 Sep 2020
+
+@author: setsu
+@filename: Tutorial_csharp.cs
+@coding: C#
+========================
+Date          Comment
+========================
+09062020      First revision
+**/
+
+// Example of C# program
+
+using System; // instruction to compiler to tell we want to use things from namespace
+
+class GlazerCalc // class is a container that holds data and program code. Every class needs an identifier (name)
+{ 
+    // Example code
+    void CalcGlaze() 
+    {
+        double width, height, woodLength, glassArea;
+        string widthString, heightString;
+
+        // Constants
+        const double MAX_WIDTH = 20.0;
+        const double MIN_WIDTH = 0.5;
+        const double MAX_HEIGHT = 250.0;
+        const double MIN_HEIGHT = 1.0;
+
+        Console.Write("Input window width: ");
+        widthString = Console.ReadLine();
+        width = double.Parse(widthString);
+
+        Console.Write("Input window height: ");
+        heightString = Console.ReadLine();
+        height = double.Parse(heightString);
+
+        if (width < MIN_WIDTH)
+        {
+            Console.WriteLine("input value less than min width value");
+            width = MIN_WIDTH;
+        } 
+        else if (width > MAX_WIDTH) 
+        { 
+            Console.WriteLine("input value more than max width value");        
+            width = MAX_WIDTH;
+        }
+
+        if (height < MIN_HEIGHT)
+        {
+            Console.WriteLine("input value less than min height value");
+            height = MIN_HEIGHT;
+        }
+        else if (height > MAX_HEIGHT)
+        {
+            Console.WriteLine("input value more than max height value");
+            height = MAX_HEIGHT;
+        }
+
+        woodLength = 2 * (width + height) * 3.25;
+        glassArea = 2 * (width + height);
+
+        Console.WriteLine("The length of the wood is " + woodLength.ToString() + " feet");
+        Console.WriteLine("The area of the glass is " + glassArea.ToString() + " square metres");
+    }
+
+    static void Main() // main function of class, mandatory
+    {
+        GlazerCalc g1 = new GlazerCalc();
+
+        g1.CalcGlaze();
+    }
+    
+}
