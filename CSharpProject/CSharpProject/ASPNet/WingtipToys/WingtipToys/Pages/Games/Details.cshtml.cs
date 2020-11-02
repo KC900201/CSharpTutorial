902c6f823c1248327dd35fc7bd2c6dd2fbf1fd20
kc900201 <nkc900201@gmail.com>
2020-11-02 21:45:19 +0900

    New branch for learning ASPNetCore in C# - 11022020

---
+++ CSharpProject/CSharpProject/ASPNet/WingtipToys/WingtipToys/Pages/Games/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WingtipToys.Data;
+using WingtipToys.Models;
+
+namespace WingtipToys.Pages.Games
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WingtipToys.Data.AppDbContext _context;
+
+        public DetailsModel(WingtipToys.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
